358436a8af9bc7bac3fc8e18bfdf4f90e722398d
Nolan <60010605+nolngo@users.noreply.github.com>
2020-10-07 18:57:03 -0700

    added missing semicolons

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,111 +1,113 @@
-const itemList = document.getElementById('item-list')
-const cartQty = document.getElementById('cart-qty')
-const cartTotal = document.getElementById('cart-total')
-const addForm = document.getElementById('add-form')
-const itemName = document.getElementById('item-name')
-const itemPrice = document.getElementById('item-price')
+const itemList = document.getElementById('item-list');
+const cartQty = document.getElementById('cart-qty');
+const cartTotal = document.getElementById('cart-total');
+const addForm = document.getElementById('add-form');
+const itemName = document.getElementById('item-name');
+const itemPrice = document.getElementById('item-price');
 
 
-itemList.innerHTML = '<li> Hello World</li>'
-console.log(itemList)
+//itemList.innerHTML = '<li> Hello World</li>'
+//console.log(itemList)
 
 
-const itemsContainer = document.getElementById("items")
+const itemsContainer = document.getElementById("items");
 
-import data from "./data.js"
+import data from "./data.js";
 
 // the length of our data determines how many times this loop goes around
 for (let i=0; i<data.length; ++i) {
     // create a new div element and give it a class name
     let newDiv = document.createElement('div');
-    newDiv.className = 'item'
+    newDiv.className = 'item';
 
     // create an image element
     let img = document.createElement('img');
     // this will change each time we go through the loop. Can you explain why?
-    img.src = data[i].image
-    img.width = 300
-    img.height = 300
+    img.src = data[i].image;
+    img.width = 300;
+    img.height = 300;
     // Add the image to the div
-    newDiv.appendChild(img)
+    newDiv.appendChild(img);
 
 
     // create a paragraph element for a description
-    let desc = document.createElement('P')
+    let desc = document.createElement('P');
     // give the paragraph text from the data
-    desc.innerText =data[i].desc
+    desc.innerText =data[i].desc;
     // append the paragraph to the div
-    newDiv.appendChild(desc)
+    newDiv.appendChild(desc);
     // do the same thing for price
-    let price = document.createElement('P')
-    price.innerText = data[i].price
-    newDiv.appendChild(price)
+    let price = document.createElement('P');
+    price.innerText = data[i].price;
+    newDiv.appendChild(price);
 
 
-    let button = document.createElement('button')
-    button.id = data[i].name
+    let button = document.createElement('button');
+    button.id = data[i].name;
 
-    button.dataset.price = data[i].price
-    button.innerHTML = "Add to Cart"
-    newDiv.appendChild(button)
+    button.dataset.price = data[i].price;
+    button.innerHTML = "Add to Cart";
+    newDiv.appendChild(button);
 
-    itemsContainer.appendChild(newDiv)
+    itemsContainer.appendChild(newDiv);
 }
 
-const all_items_button = Array.from(document.querySelectorAll("button"))
-all_items_button.forEach(elt => elt.addEventListener('click', () => {
-  addItem(elt.getAttribute('id'), elt.getAttribute('data-price'))
-  showItems()
-}))
+const all_items_button = Array.from(document.querySelectorAll("button"));
+all_items_button.forEach(elt =>
+    elt.addEventListener('click', () => {
+        addItem(elt.getAttribute('id'), elt.getAttribute('data-price'));
+        showItems();
+    })
+);
 
 const cart = []
 
 itemList.onchange = function (e) {
     if (e.target && e.target.classList.contains('update')) {
-        const name = e.target.dataset.name
-        const qty = parseInt(e.target.value)
-        updateCart(name, qty)
+        const name = e.target.dataset.name;
+        const qty = parseInt(e.target.value);
+        updateCart(name, qty);
     }
-}
+};
 
 itemList.onclick = function (e) {
     // console.log("Clicked List!")
     // console.log(e.target)
     if (e.target && e.target.classList.contains('remove')) {
-        const name = e.target.dataset.name // date-name = "???"
-        removeItems()
+        const name = e.target.dataset.name; // date-name = "???"
+        removeItems();
     } else if (e.target && e.target.classList.contains('add-one')) {
-      const name = e.target.dataset.name
-      addItem(name)
+      const name = e.target.dataset.name;
+      addItem(name);
     } else if (e.target && e.target.classList.contains('remove-one')) {
-      const name = e.target.dataset.name
-      removeItem(name, 1)
+      const name = e.target.dataset.name;
+      removeItem(name, 1);
     }
-}
+};
 addForm.onsubmit = function(e) {
-    e.preventDefault()
-    const name = itemName.value
-    const price = itemPrice.value
-    addItem(name, price)
-  }
+    e.preventDefault();
+    const name = itemName.value;
+    const price = itemPrice.value;
+    addItem(name, price);
+};
 
 
 function addItem(name, price, qty) {
     for (let i = 0; i < cart.length; i += 1) {
         if (cart[i].name === name) {
-            cart[i].qty += 1
-            return
-            }
+            cart[i].qty += 1;
+            return;
+        }
     }
-    const item = {name, price, qty: 1}
-    cart.push(item)
+    const item = {name, price, qty: 1};
+    cart.push(item);
 }
 
 function showItems() {
-    const qty = getQty()
-    cartQty.innerHTML = `You have ${qty} items in your cart.`
-    let itemStr = ''
-
+    const qty = getQty();
+    cartQty.innerHTML = `You have ${qty} items in your cart.`;
+    
+    let itemStr = "";
     for (let i = 0; i < cart.length; i += 1) {
         itemStr += `<li>
         ${name} $${price} x ${qty} = ${qty * price} 
@@ -113,43 +115,44 @@ function showItems() {
         <button class="add-one" data-name=${name}> + </button>
         <button class="remove-one" data-name=${name}> - </button>
         <input class="update" type="number" min="0" data-name="${name}>
-        </li>`
+        </li>`;
         }
 
 
-    itemList.innerHTML = itemStr
-    cartTotal.innerHTML = `Total in cart: $${getTotal()}`
+    itemList.innerHTML = itemStr;
+    cartTotal.innerHTML = `Total in cart: $${getTotal()}`;
     }
 
 
   // Get Quantity.
 function getQty() {
-    let qty = 0
+    let qty = 0;
     for (let i = 0; i < cart.length; i += 1) {
-      qty += cart[i].qty
+      qty += cart[i].qty;
     }
-    return qty
+    return qty;
   }
 
   // Get Total.
 function getTotal() {
-    let total = 0
+    let total = 0;
     for (let i = 0; i < cart.length; i += 1) {
-      total += cart[i].price * cart[i].qty
+      total += cart[i].price * cart[i].qty;
     }
-    return total.toFixed(2)
+    return total.toFixed(2);
 }
 
 function removeItems (name, qty = 0) {
     for (let i = 0; i < cart.length; i += 1) {
       if (cart[i].name === name) {
         if (qty > 0) {
-          cart[i].qty -= qty
+            cart[i].qty -= qty;
         }
         if (cart[i].qty < 1 || qty === 0) {
-          cart.splice(i, 1)
+            cart.splice(i, 1);
         }
-        return
+        showItems();
+        return;
       }
     }
   }
@@ -158,12 +161,12 @@ function removeItems (name, qty = 0) {
     for (let i = 0; i < cart.length; i += 1) {
       if (cart[i].name === name) {
         if (qty < 1) {
-          removeItem(name)
-          return
+          removeItem(name);
+          return;
         }
-        cart[i].qty = qty
-        showItems()
-        return
+        cart[i].qty = qty;
+        showItems();
+        return;
       }
     }
   }
